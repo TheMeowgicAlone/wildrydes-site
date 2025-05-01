21f74349dfc384983f68b434fd47a71167e414ac
TheMeowgicAlone <themeowgicalone@gmail.com>
2025-05-01 17:35:39 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://cognito-idp.us-east-1.amazonaws.com/us-east-1_U9OPcTtXU/.well-known/jwks.json' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://3ec75xqpnf.execute-api.us-east-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
